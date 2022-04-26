c8b8bd9502a0aba6c5084470313f32a17a5ba01b
saif955 <99525405+saif955@users.noreply.github.com>
2022-04-26 19:31:25 +0600

    Add files via upload

---
+++ links.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+
+
+<style>
+    .test_php{
+      display: inline-block;
+      width: 20%;
+      padding: 10px 5px;
+      margin: 2%;
+      margin-left: 50px;
+      box-shadow: 0 0 1px 5px thistle;
+      text-align: center;
+      color: #599532; 
+      
+    }
+    .test_php p{
+        display: block;
+  margin-left: 30px;
+  margin-right: 10px;
+  width: 85%
+       
+    }
+   
+
+</style>
+
+<div class='test_php'>
+<h1></h1>
+
+<a href="testfile.php">
+         <img alt="Navi" src="https://cdn.dribbble.com/users/4458651/screenshots/15907636/navi_page_1.jpg"
+         width="150" height="70">
+<?php
+?>
+</div>
+
+</body>
+</html>
